55e788de44c1e3c138d91f8364ddfbf14069fb11
Paul Sherrill <pauldsherrill@yahoo.com>
2024-04-07 21:18:57 -0500

    added appending capabilities

--- Assets/file.js
+++ Assets/file.js
@@ -1,10 +1,10 @@
 const searchedEl = document.getElementById('searched-cities');
-let city = document.getElementById('city').value;
+const searchButton = document.getElementById('search');
+const currentWeather = document.getElementById('current-weather');
 
 function getWeather(city) {
-    let enteredCity = city;
     fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${city}`,
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=06e0ad8aa9e344a8a546262413b9bc86&units=imperial`,
       {
         method: "GET",
       }
@@ -15,11 +15,40 @@ function getWeather(city) {
       .then(function (data) {
         console.log(data);
   
-        console.log(`City: ${enteredCity}`);
-        console.log(`Temp: ${data.main.temp}`);
-        console.log(`Wind: ${data.wind.speed}`);
-        console.log(`Temp: ${data.main.humidity}`);
+        let city = {
+            name: `${data.name}`,
+            temp: `Temp: ${data.main.temp}`,
+            wind: `Wind: ${data.wind.speed}`,
+            humidity: `Humidity: ${data.main.humidity}`
+          }
+        
+        console.log(city);
+        createWeatherBox(city);
       });
 }
 
-getWeather();
+searchButton.addEventListener("click", function () {
+    let city = document.getElementById('city').value;
+    getWeather(city);
+  });
+
+function createWeatherBox(city) {
+    currentWeather.style.border = "solid black"
+
+    let title = document.createElement("h2");
+    title.textContent = city.name;
+    currentWeather.appendChild(title);
+
+    let temperature = document.createElement("p");
+    temperature.textContent = city.temp + " °F";
+    currentWeather.appendChild(temperature);
+
+    let wind = document.createElement("p");
+    wind.textContent = city.wind + " mph";
+    currentWeather.appendChild(wind);
+
+    let humidity = document.createElement("p");
+    humidity.textContent = city.humidity + "%";
+    currentWeather.appendChild(humidity);
+}
+
