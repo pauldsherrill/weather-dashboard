4c242aca6b52195bdcbdfbfdd54ec246ef4be31f
Paul Sherrill <pauldsherrill@yahoo.com>
2024-04-07 22:36:27 -0500

    setting up local storage

--- Assets/file.js
+++ Assets/file.js
@@ -22,9 +22,20 @@ function getWeather(city) {
             humidity: `Humidity: ${data.main.humidity}`,
             icon: data.weather[0].icon
           }
+
+          let cities = JSON.parse(localStorage.getItem('city'));
+
+          if (cities) {
+            cities.push(city);
+            localStorage.setItem('city', JSON.stringify(cities));
+          } else {
+            const citiesInitializer = [city];
+            localStorage.setItem('city', JSON.stringify(citiesInitializer));
+          }
         
         console.log(city);
         createWeatherBox(city);
+
       });
 }
 
@@ -38,8 +49,14 @@ function createWeatherBox(city) {
     currentWeather.style.border = "solid black"
 
     let title = document.createElement("h2");
-    title.textContent = city.name + city.icon;
+    title.textContent = city.name;
+    let iconImage = document.createElement("img");
+    iconImage.setAttribute(
+        'src',
+        `https://openweathermap.org/img/wn/${city.icon}.png`
+    );
     currentWeather.appendChild(title);
+    title.appendChild(iconImage);
 
     let temperature = document.createElement("p");
     temperature.textContent = city.temp + " °F";
@@ -52,5 +69,9 @@ function createWeatherBox(city) {
     let humidity = document.createElement("p");
     humidity.textContent = city.humidity + "%";
     currentWeather.appendChild(humidity);
+}
+
+function addToSearchHistory() {
+    localStorage.setItem('city', JSON.stringify(city));
 }
 
