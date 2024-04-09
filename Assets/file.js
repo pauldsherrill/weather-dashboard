f932da87c63cb4054b1b3bee58c82cbb1c64f3ed
Paul Sherrill <pauldsherrill@yahoo.com>
2024-04-08 23:03:41 -0500

    finished js and html

--- Assets/file.js
+++ Assets/file.js
@@ -4,6 +4,7 @@ const currentWeather = document.getElementById('current-weather');
 const searchHistory = document.getElementById('search-history');
 const forecast = document.getElementById('forecast');
 
+// The main function that runs to retrieve weather information
 function getWeather(city) {
     fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4f6699c207bc8fb45b36a8165870586c&units=imperial`,
@@ -15,6 +16,7 @@ function getWeather(city) {
         return response.json();
       })
       .then(function (data) {
+        console.log(data);
         let city = {
             name: `${data.name}`,
             temp: `Temp: ${data.main.temp}`,
@@ -26,17 +28,11 @@ function getWeather(city) {
             dt: data.dt,
           }
         
-          let cities = JSON.parse(localStorage.getItem('city')) || [];
-          cities.push(data.name);
+        // Stores cities to local storage so they can be saved in the search history
+        let cities = JSON.parse(localStorage.getItem('city')) || [];
+        cities.push(data.name);
+        localStorage.setItem('city', JSON.stringify(cities));
 
-          //if (Array.isArray(cities) === true) {
-            //cities.push(city.name);
-            //localStorage.setItem('city', JSON.stringify(cities));
-         // } else {
-           // const citiesInitializer = [city.name];
-           // localStorage.setItem('city', JSON.stringify(citiesInitializer));
-         // }
-        
         pageLoad();
         createWeatherBox(city);
         getForecast(city);
@@ -47,11 +43,12 @@ searchButton.addEventListener("click", function () {
     handleSearchButton();
   });
 
+// Adds all the needed text and styling to the current weather box 
 function createWeatherBox(city) {
     currentWeather.style.border = "solid black"
 
     let title = document.createElement("h2");
-    title.textContent = city.name;
+    title.textContent = city.name + " (" + dayjs.unix(city.dt).format('M/D/YYYY') + ") ";
     let iconImage = document.createElement("img");
     iconImage.setAttribute(
         'src',
@@ -73,7 +70,9 @@ function createWeatherBox(city) {
     currentWeather.appendChild(humidity);
 }
 
+// Retrieves data for the 5 day forecast information 
 function getForecast(city) {
+    // Fetches the 5 day forecast API using the coordinates for the city found in the previous API
     fetch(
         `https://api.openweathermap.org/data/3.0/onecall?lat=${city.lat}&lon=${city.lon}&appid=4f6699c207bc8fb45b36a8165870586c&units=imperial`,
         {
@@ -84,7 +83,10 @@ function getForecast(city) {
         return response.json();
       })
       .then(function (data) {
-        for (i = 0; i < 5; i++) {
+        console.log(data);
+
+        // Loops through five days of forecast. Starting on the day after the current date
+        for (i = 1; i < 6; i++) {
             let city = {
                 temp: `Temp: ${data.daily[i].temp.day}`,
                 wind: `Wind: ${data.daily[i].wind_speed}`,
@@ -92,26 +94,23 @@ function getForecast(city) {
                 icon: data.daily[i].weather[0].icon,
                 dt: data.daily[i].dt,
               }
-    
+
             createForecastBox(city);
         }
       });
 }
 
+// Renders the daily forecast into containers, adding the proper text and styling
 function createForecastBox(city) {
     let dailyForecast = document.createElement("div");
     dailyForecast.setAttribute (
-        'class',
-        'border my-0'
-    );
-    dailyForecast.setAttribute (
         'style',
-        'width: 20%; height: 330px; background-color: blue; padding-top: 10px;'
+        'width: 18%; height: 250px; background-color: #635E88; padding-top: 10px; color: white; margin: 0 1% 0 1%'
     );
     forecast.appendChild(dailyForecast);
 
     let title = document.createElement("h3");
-    title.textContent = city.dt;
+    title.textContent = dayjs.unix(city.dt).format('M/D/YYYY');
     dailyForecast.appendChild(title);
 
     let iconImage = document.createElement("img");
@@ -134,23 +133,49 @@ function createForecastBox(city) {
     dailyForecast.appendChild(humidity);
 }
 
+// Does the same thing as getWeather except re-add the city to the search history or re-save the city into local storage
 function handleCityButton(storedCity) {
     currentWeather.innerHTML = "";
-    let city = storedCity.name;
-    getWeather(city);
-    forecast.innerHTML = `<h2 class="pt-4">5 Day Forecast</div>`;
+    let city = storedCity;
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4f6699c207bc8fb45b36a8165870586c&units=imperial`,
+      {
+        method: "GET",
+      }
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        let city = {
+            name: `${data.name}`,
+            temp: `Temp: ${data.main.temp}`,
+            wind: `Wind: ${data.wind.speed}`,
+            humidity: `Humidity: ${data.main.humidity}`,
+            icon: data.weather[0].icon,
+            lat: data.coord.lat,
+            lon: data.coord.lon,
+            dt: data.dt,
+          }
+        
+        createWeatherBox(city);
+        getForecast(city);
+      });
+    forecast.innerHTML = `<h2 class="pt-4">5-Day Forecast:</div>`;
 }
 
+// Adds all the needed information when the search button is clicked, including resetting the html so that the previous info is replaced with new info
 function handleSearchButton() {
     currentWeather.innerHTML = "";
     let city = document.getElementById('city').value;
     getWeather(city);
-    forecast.innerHTML = `<h2 class="pt-4">5 Day Forecast</div>`;
+    forecast.innerHTML = `<h2 class="pt-4">5-Day Forecast:</div>`;
 }
 
+// Adds buttons to the search history for each object saved in the local storage
 function pageLoad() {
     const storedCities = JSON.parse(localStorage.getItem('city')) || [];
-    storedCities.innerHTML = "";
+    searchHistory.innerHTML = "";
 
     for (const storedCity of storedCities) {
         let searchedCity = document.createElement("button");
@@ -172,8 +197,9 @@ function pageLoad() {
         searchedCity.addEventListener('click', function() {
             handleCityButton(storedCity);
         });
-        }
+    }
 }
 
+// Keeps the search history on the page when refreshed
 pageLoad();
 
---
+++ README.md
@@ -0,0 +1,26 @@
+# Weather Dashboard
+
+## Description
+
+In this challenge, I was tasked with creating a webpage that rerieves data about the weather in an inputted city. This data had to include current weather regarding the weather conditions, temperature, wind speed, and humidity. It also had ot include a 5 day forecast with the previously mentioned information for each of the next 5 days. I was also tasked with storing previously searched city on the page as search history where the user could re-view weather information in a city they had already searched.
+
+## Installation
+
+To install my code, I started with making the html file for the first page. I added a header as well as an input element and search button for the user to input a city to search. I then created various div elements where I would be appending the information coming from API databases and local storage. From there, I went ahead and added placeholder elements to match the styling of the finished page I had in mind. I used this later on when adding styling to elements created in the js file. 
+
+I then began working on the js file by fetching the first API I wanted to use for the current weather data. I then created a variable to store the needed data called 'city'. I then worked on creating a function to append that data into the html. I copied the styling I had already added to the html and moved it to my js file to do so. The next step was to retrieve the input from the input element in the html. To do so, I created an event listener to retrieve that input from a variable when the search button was clicked. I then added thatvariable into the API URL so that it would retrieve the data for that specific user inputted city. The next step was to create a function to get retrieve the data for the 5 day forecast. I did so by adding another API and implementing the coordinates fetched the from the previous API database so the cities would match. I then once again retrieved the needed data for each day and used a for loop to make sure it only retrieved from the following 5 days. My next step was to once again append that data into the html., so I created a function and copied the styling from the html and moved it to the js file. Once I saw that everything was working, my next step was to create the search history, so I went back to the frirst function to store the name of the city to an array in local storage. Now that things were being stored there, I then created a function with a for loop to append each city name in the array to the html. Finally, I finished by creating an event listener so that when a city in the search history was clicked, a function to display the weather data was run. I did this by using my first function but changing it slightly so that it didn't restore the city into local storage or re-add the city to the search history. 
+
+## Usage
+
+CLick on the link to see my deployed webpage 
+
+Once you arrive to my website you will an area where you can search for a city. 
+
+![alt text](./Assets/emptyform.png)
+
+When you enter a city and click search, a display of the current weather in that city as well as a 5-day forecast for that city are displayed. The city is also stored in the search history to revisit. 
+
+![alt text](./Assets/display.png)
+
+When another city is searched for, the page will be replaced with a display of that city's weather data and that city will also be added to the search history and so on and so forth. When a previous city is clicked on in the search history, the weather data is then shown again for that city.
+
